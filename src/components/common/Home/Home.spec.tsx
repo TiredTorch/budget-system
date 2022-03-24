109f7928e0d73f94d8a4498f57ff8ba0d817d8fd
Tr.Torch <vitochke337@gmail.com>
2022-03-24 02:33:12 +0200

    Added test for Home component, which checks values, added with new spend

--- src/components/common/Home/Home.spec.tsx
+++ src/components/common/Home/Home.spec.tsx
@@ -1,12 +1,14 @@
-import {render, screen} from '@testing-library/react';
+import {render, screen, waitFor} from '@testing-library/react';
 import React, {SetStateAction} from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import {Home} from './Home';
 import '@testing-library/jest-dom';
 import {ISpend} from 'types/types';
+import userEvent from '@testing-library/user-event';
 
 describe('Testing Home component', () => {
-  test('should add new spend', () => {
+  test('should add new spend with proper values', async () => {
+    const handleAddSpend = jest.fn();
     render(
         <BrowserRouter>
           <Home
@@ -14,9 +16,7 @@ describe('Testing Home component', () => {
             logOut={(): void => {
               throw new Error('Function not implemented.');
             } }
-            addSpend={(values: any): Promise<void> => {
-              throw new Error('Function not implemented.');
-            } }
+            addSpend={handleAddSpend}
             deleteSpend={(spend: ISpend): Promise<void> => {
               throw new Error('Function not implemented.');
             } }
@@ -31,5 +31,17 @@ describe('Testing Home component', () => {
     );
     const currentSpends = screen.getByText('Your spends list is empty');
     expect(currentSpends).toBeInTheDocument();
+
+    userEvent.type(screen.getByLabelText(/What did you/i), 'Toys');
+    userEvent.type(screen.getByLabelText(/How much/i), '22');
+
+    userEvent.click(screen.getByText(/Add spend/i));
+
+    await waitFor(() =>
+      expect(handleAddSpend).toHaveBeenCalledWith({
+        spendItem: 'Toys',
+        spendCost: 22,
+      }),
+    );
   });
 });
