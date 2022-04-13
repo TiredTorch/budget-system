04221c7f69cf5a0e2dc063176c8d2170ae0ecd91
Tr.Torch <vitochke337@gmail.com>
2022-04-13 11:52:15 +0300

    Added lazy loading

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import {FC} from 'react';
+import {FC, Suspense} from 'react';
 import {Route, Routes} from 'react-router';
 import {Layout} from 'layouts/Layout';
 import {IAppRoutesProps} from 'types/types';
@@ -7,22 +7,25 @@ import {getBudgetState} from 'contexts/BudgetContext';
 export const AppRoutes : FC<IAppRoutesProps> = ({routes}) => {
   const budgetState = getBudgetState();
   return (
-    <Routes>
-      {routes.map((route) => {
-        if (!!route.auth !== !!budgetState?.user) return;
+    <Suspense fallback={<h3>Loading...</h3>}>
+      <Routes>
+        {routes.map((route) => {
+          if (!!route.auth !== !!budgetState?.user) return;
 
-        return (
-          <Route
-            path={route.path}
-            key={`route ${route.path}`}
-            element={
-              <Layout>
-                <route.page/>
-              </Layout>
-            }
-          />
-        );
-      })}
-    </Routes>
+          return (
+            <Route
+              path={route.path}
+              key={`route ${route.path}`}
+              element={
+                <Layout>
+                  <route.page/>
+                </Layout>
+              }
+            />
+          );
+        })}
+      </Routes>
+    </Suspense>
+
   );
 };
--- src/routes/routesList.ts
+++ src/routes/routesList.ts
@@ -1,7 +1,15 @@
-import {HomePage} from 'pages/HomePage';
-import {LoginPage} from 'pages/LoginPage';
-import {LogupPage} from 'pages/LogupPage';
+import {lazy} from 'react';
 import {IRoute} from 'types/types';
+
+const LoginPage = lazy(() =>
+  import('pages/LoginPage').then((module) => ({default: module.LoginPage})),
+);
+const LogupPage = lazy(() =>
+  import('pages/LogupPage').then((module) => ({default: module.LogupPage})),
+);
+const HomePage = lazy(() =>
+  import('pages/HomePage').then((module) => ({default: module.HomePage})),
+);
 
 export const routes: Array<IRoute> = [
   {
